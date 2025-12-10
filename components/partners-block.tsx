7162f934f8164f3d9b65b78fcce7c505cee48875
Matt Reynolds <matt@matterwebsolutions.com>
2025-12-10 20:01:15 +0000

    Update partners-block.tsx
    Add justify center

--- components/partners-block.tsx
+++ components/partners-block.tsx
@@ -4,7 +4,7 @@ export function PartnersBlock() {
   return (
     <div className="flex flex-col gap-4 w-full">
       <h2 className="text-3xl font-serif">Our Partners:</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 items-center">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 items-center justify-center">
         <Image src="/partners/sykes.svg" alt="Sykes Holiday Cottages" width={300} height={100} />
         <Image src="/partners/oyster.svg" alt="Oyster Holiday Cottages" width={300} height={100} />
         <Image src="/partners/Anglesey.svg" alt="Anglesey Holiday Lettings" width={300} height={100} />
