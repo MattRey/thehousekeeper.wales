7a635967811d9639c2887f29a3afaa1cf26e2b92
Matt Reynolds <mattreynolds@qogita.com>
2025-12-11 12:26:01 +0000

    chore: update partner's logo sizes

--- components/partners-block.tsx
+++ components/partners-block.tsx
@@ -5,12 +5,12 @@ export function PartnersBlock() {
     <div className="flex flex-col gap-8 w-full">
       <h2 className="text-3xl font-serif">Our Partners:</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 items-center">
-        <Image src="/partners/sykes.svg" alt="Sykes Holiday Cottages" width={300} height={100} className="justify-self-center"/>
-        <Image src="/partners/oyster.svg" alt="Oyster Holiday Cottages" width={300} height={100} className="justify-self-center" />
-        <Image src="/partners/Anglesey.svg" alt="Anglesey Holiday Lettings" width={300} height={100} className="justify-self-center" />
-        <Image src="/partners/cottages.svg" alt="Cottages.com" width={300} height={100} className="justify-self-center" />
-        <Image src="/partners/menai.svg" alt="Menai Holiday Cottages" width={300} height={100} className="justify-self-center" />
-        <Image src="/partners/nwhc.svg" alt="North Wales Holiday Cottages" width={300} height={100} className="justify-self-center" />
+        <Image src="/partners/sykes.svg" alt="Sykes Holiday Cottages" width={220} height={100} className="justify-self-center"/>
+        <Image src="/partners/oyster.svg" alt="Oyster Holiday Cottages" width={210} height={100} className="justify-self-center" />
+        <Image src="/partners/Anglesey.svg" alt="Anglesey Holiday Lettings" width={200} height={100} className="justify-self-center" />
+        <Image src="/partners/cottages.svg" alt="Cottages.com" width={230} height={100} className="justify-self-center" />
+        <Image src="/partners/menai.svg" alt="Menai Holiday Cottages" width={180} height={100} className="justify-self-center" />
+        <Image src="/partners/nwhc.svg" alt="North Wales Holiday Cottages" width={230} height={100} className="justify-self-center" />
       </div>
     </div>
   );
